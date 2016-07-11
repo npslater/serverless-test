b2694f38229b03cba80ffffc2eb54484b4dc4c40
Nate Slater <nateslate@gmail.com>
2016-07-10 18:27:16 -0700

    created s3_helper and added handler

--- functions/s3EventHandler/handler.js
+++ functions/s3EventHandler/handler.js
@@ -1,7 +1,5 @@
 'use strict';
 
-module.exports.handler = function(event, context, cb) {
-  return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  });
+module.exports.handler = function(event, context) {
+  context.succeed();
 };
--- functions/s3EventHandler/lib/s3_helper.js
+++ functions/s3EventHandler/lib/s3_helper.js
@@ -1,3 +1,19 @@
 function S3Helper(client) {
   this._client = client;
 }
+
+S3Helper.prototype.getObject = function(params) {
+  return new Promise(
+    (resolve, reject) => {
+      this._client.getObject(params, (err, response) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(response);
+      });
+    }
+  );
+};
+
+module.exports = S3Helper;
